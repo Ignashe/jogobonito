369425ef5e8db67457fbb324a38c4fbcd015c7d8
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-03-18 12:25:53 +0100

    animacions

--- Escenes/cos.gd
+++ Escenes/cos.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 var velocitat_base = 200
 var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
+var salt = Vector2.UP * 400
+var doblejump = 1
 
 func _physics_process(delta):
 	velocitat.x = 0
@@ -10,8 +12,16 @@ func _physics_process(delta):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("mou esquerra"):
 		velocitat += Vector2.LEFT * velocitat_base
+	if is_on_floor():
+		doblejump = 2
+	if Input.is_action_just_pressed("mou adalt") and doblejump > 0:
+		doblejump -= 1
+		velocitat.y = 0
+		velocitat += salt
+	if position.y >= 1000:
+		position = Vector2(59,304)
 	velocitat += gravetat * delta
-	velocitat = move_and_slide(velocitat)
+	velocitat = move_and_slide(velocitat, Vector2.UP)
 
 
 
