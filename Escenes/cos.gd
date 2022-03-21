82558e4de2150d753c5f371f79e2e5d34e8b98c7
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-03-21 12:22:31 +0100

    escena

--- Escenes/cos.gd
+++ Escenes/cos.gd
@@ -5,23 +5,37 @@ var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 400
 var doblejump = 1
-
+var altura = 0
 func _physics_process(delta):
 	velocitat.x = 0
 	if Input.is_action_pressed("mou dreta"):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("mou esquerra"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if is_on_floor():
-		doblejump = 2
-	if Input.is_action_just_pressed("mou adalt") and doblejump > 0:
+	if Input.is_action_just_pressed("mou adalt") and doblejump != 0:
 		doblejump -= 1
 		velocitat.y = 0
 		velocitat += salt
+	if is_on_floor():
+		doblejump = 1
 	if position.y >= 1000:
 		position = Vector2(59,304)
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 
-
-
+	anima(velocitat)
+func anima(velocitat: Vector2):
+	var animacio : AnimatedSprite = $AnimatedSprite
+	if velocitat.x > 0:
+		animacio.flip_h = false
+		animacio.play('camina')
+	elif velocitat.x < 0:
+		animacio.flip_h = true
+		animacio.play('camina')
+	if velocitat.y < -1:
+		animacio.play('salta')
+		return
+	if abs(velocitat.x) < 0.1:
+		animacio.play('quiet')
+func _on_Final_body_enered(body):
+	get_tree().change_scene('')
