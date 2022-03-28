f18e643be43ac121ea389ba22f95001651ed7943
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-03-28 12:23:42 +0200

    Millora de la escala

--- Escenes/cos.gd
+++ Escenes/cos.gd
@@ -5,20 +5,24 @@ var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 400
 var doblejump = 1
-var altura = 0
-func _on_Area2D_body_entered(body):
-		if Input.is_action_just_pressed("mou adalt"):
-			velocitat = Vector2.UP * 10
+var escala = false
+var velocitat_escala = Vector2.ZERO
+
 func _physics_process(delta):
 	velocitat.x = 0
 	if Input.is_action_pressed("mou dreta"):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("mou esquerra"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_just_pressed("mou adalt") and doblejump != 0:
-		doblejump -= 1
-		velocitat.y = 0
-		velocitat += salt
+	if Input.is_action_just_pressed("mou adalt"):
+		if escala == true:
+			velocitat_escala += Vector2.UP * 400
+		if escala == false:
+			if doblejump  == 1:
+				velocitat += Vector2.UP * 400
+				doblejump -= 1
+				velocitat.y = 0
+				velocitat += salt
 	if is_on_floor():
 		doblejump = 1
 	if position.y >= 1000:
@@ -26,7 +30,6 @@ func _physics_process(delta):
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	anima(velocitat)
-
 func anima(velocitat: Vector2):
 	var animacio : AnimatedSprite = $AnimatedSprite
 	if velocitat.x > 0:
@@ -35,9 +38,15 @@ func anima(velocitat: Vector2):
 	elif velocitat.x < 0:
 		animacio.flip_h = true
 		animacio.play('Camina')
+	if escala == true:
+		animacio.play('Escala1')
 	if velocitat.y < -100:
-		animacio.play('Salta')
-		return
+		if escala == true:
+			animacio.play('Escala')
+			return
+		if escala == false:
+			animacio.play('Salta')
+			return
 	if velocitat.y > 500:
 		animacio.play('Cau')
 		return
@@ -46,4 +55,11 @@ func anima(velocitat: Vector2):
 
 
 
+func _on_Escala_body_entered(body):
+	escala = true
+	print('a')
 
+
+func _on_Escala_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	escala = false
+	print('b')
