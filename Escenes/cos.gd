ba5e58df56d137dcbbf8e2bb32dcd9de0865d9c3
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-03-30 08:54:21 +0200

    Escala no acaba de funcionar

--- Escenes/cos.gd
+++ Escenes/cos.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var velocitat_base = 200
+var velocitat_base_escala = 200
 var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 400
@@ -10,26 +11,33 @@ var velocitat_escala = Vector2.ZERO
 
 func _physics_process(delta):
 	velocitat.x = 0
-	if Input.is_action_pressed("mou dreta"):
-		velocitat += Vector2.RIGHT * velocitat_base
-	if Input.is_action_pressed("mou esquerra"):
-		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_just_pressed("mou adalt"):
-		if escala == true:
-			velocitat_escala += Vector2.UP * 400
-		if escala == false:
+	if escala == false:
+		if Input.is_action_pressed("mou dreta"):
+			velocitat += Vector2.RIGHT * velocitat_base
+		if Input.is_action_pressed("mou esquerra"):
+			velocitat += Vector2.LEFT * velocitat_base
+		if Input.is_action_just_pressed("mou adalt"):
 			if doblejump  == 1:
 				velocitat += Vector2.UP * 400
 				doblejump -= 1
 				velocitat.y = 0
 				velocitat += salt
-	if is_on_floor():
-		doblejump = 1
-	if position.y >= 1000:
-		position = Vector2(45,394)
-	velocitat += gravetat * delta
+		if is_on_floor():
+			doblejump = 1
+		if position.y >= 1000:
+			position = Vector2(45,394)
+		velocitat += gravetat * delta
+		anima(velocitat)
+	if escala == true:
+		if Input.is_action_just_pressed("mou adalt"):
+			velocitat_escala += Vector2.UP * velocitat_base_escala
+		if Input.is_action_pressed("mou dreta"):
+			velocitat += Vector2.RIGHT * velocitat_base
+		if Input.is_action_pressed("mou esquerra"):
+			velocitat += Vector2.LEFT * velocitat_base
+		velocitat_escala = move_and_slide(velocitat_escala,Vector2.UP)
+		anima(velocitat_escala)
 	velocitat = move_and_slide(velocitat, Vector2.UP)
-	anima(velocitat)
 func anima(velocitat: Vector2):
 	var animacio : AnimatedSprite = $AnimatedSprite
 	if velocitat.x > 0:
@@ -38,15 +46,9 @@ func anima(velocitat: Vector2):
 	elif velocitat.x < 0:
 		animacio.flip_h = true
 		animacio.play('Camina')
-	if escala == true:
-		animacio.play('Escala1')
 	if velocitat.y < -100:
-		if escala == true:
-			animacio.play('Escala')
-			return
-		if escala == false:
-			animacio.play('Salta')
-			return
+		animacio.play('Salta')
+		return
 	if velocitat.y > 500:
 		animacio.play('Cau')
 		return
